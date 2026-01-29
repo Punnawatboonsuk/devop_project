e400e66428211d3a2addbf2240aed74ca1e245f8
NaTiSive <Nattakorn4082@gmail.com>
2026-01-29 20:46:19 +0700

    create staff page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Layout from './components/Layout';
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./components/Layout";
 // Import Pages ที่สร้างใหม่
-import StudentDashboard from './pages/student/Dashboard';
-import CreateTicket from './pages/student/CreateTicket';
+import StudentDashboard from "./pages/student/Dashboard";
+import CreateTicket from "./pages/student/CreateTicket";
 // import TrackingPage... (ถ้าคุณทำแล้ว)
+import StaffDashboard from "./pages/staff/Dashboard";
+import ReviewTicket from "./pages/staff/ReviewTicket";
 
 function App() {
   return (
@@ -20,12 +22,24 @@ function App() {
           <Route index element={<Navigate to="dashboard" replace />} />
         </Route>
 
-        {/* --- ADMIN ROUTES (ทำทีหลัง) --- */}
-        <Route path="/admin" element={<Layout role="ADMIN" />}>
-           <Route path="dashboard" element={<h1>Admin Dashboard</h1>} />
+        <Route path="/staff" element={<Layout role="STAFF" />}>
+          <Route path="dashboard" element={<StaffDashboard />} />
+          <Route path="reviews" element={<StaffDashboard />} />{" "}
+          {/* ใช้หน้าเดียวกับ Dashboard ไปก่อน */}
+          <Route path="review/:id" element={<ReviewTicket />} />{" "}
+          {/* หน้าดูรายละเอียดแบบ Dynamic ID */}
+          <Route index element={<Navigate to="dashboard" replace />} />
         </Route>
 
-        <Route path="*" element={<Navigate to="/student/dashboard" replace />} />
+        {/* --- ADMIN ROUTES (ทำทีหลัง) --- */}
+        <Route path="/admin" element={<Layout role="ADMIN" />}>
+          <Route path="dashboard" element={<h1>Admin Dashboard</h1>} />
+        </Route>
+
+        <Route
+          path="*"
+          element={<Navigate to="/student/dashboard" replace />}
+        />
       </Routes>
     </BrowserRouter>
   );
